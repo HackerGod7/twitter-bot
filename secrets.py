1bf3781335af440f8ba4914ea298e845450c1398
LucielChoi <72686785+HackerGod7@users.noreply.github.com>
2020-11-13 18:37:49 -0800

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,5 +1,5 @@
-key = [XXXXXXX,
-      XXXXXXX,
-      XXXXXXX,
-      XXXXXXX
+key = [ FN2owua2c384eganx5plzV8PF,
+     RLX5KMs05YJZwpOyNLGtH9fy4orxZhnkiNv87GDroJbUDezsO,
+      1311729542399049729-X3bxk2vY0aj1HxooUdPQnO4FdljVFy,
+      4MVSYr5rXV0cRTSIfymyXiuwSGh3s2KfwkrsvJX30wj48
 ]
